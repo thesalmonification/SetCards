f5caa3408e42c3802ed060f5050e670bc3deb45b
thesalmonification <33437091+thesalmonification@users.noreply.github.com>
2017-11-17 15:42:41 -0600

    Build Load Cards
    This function builds all 81 Set cards...It also puts a card number 1-89 along with the Card

---
+++ LoadCards.m
@@ -0,0 +1,16 @@
+function Cards = LoadCards()
+Cards = [];
+for i = 1:3
+    for j = 1:3
+        for k = 1:3
+            for l = 1:3
+                addmat = [i,j,k,l];
+                Cards = [Cards; addmat];
+            end
+        end
+    end
+end
+size(Cards)
+Cards = [Cards,[1:81]'];
+
+end
